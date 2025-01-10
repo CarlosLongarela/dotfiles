400a622e7763321013623575f2fe848d8fd70fd1
Carlos Longarela <carlos@longarela.eu>
2025-01-10 20:44:19 +0100

    startup profiles for bash and zsh

--- install.sh
+++ install.sh
@@ -6,6 +6,15 @@ ln -sf ~/dotfiles/shells/.aliases ~/.aliases
 # Create symbolic links for .functions
 ln -sf ~/dotfiles/shells/.functions ~/.functions
 
+if [[ -n "$ZSH_VERSION" ]]; then
+    echo -e "\nZSH detected, linking startup profiles"
+    ln -sf ~/dotfiles/shells/zsh/.zlogin ~/.zlogin
+    ln -sf ~/dotfiles/shells/zsh/.zprofile ~/.zprofile
+elif [[ -n "$BASH_VERSION" ]]; then
+    echo -e "\nBash detected, linking startup profiles"
+    ln -sf ~/dotfiles/shells/bash/.bash_profile ~/.bash_profile
+fi
+
 echo "Symlinks created successfully."
 
 # Install bat
